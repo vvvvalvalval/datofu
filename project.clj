840501c43ce88255338da61b289999e923b84335
Valentin Waeselynck <val@bandsquare.fr>
2017-04-08 20:57:26 +0200

    changed artifact name and license in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject datofu "0.1.0"
+(defproject vvvvalvalval/datofu "0.1.0"
   :description "Battery-included Datomic"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/vvvvalvalval/datofu"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :profiles {:dev
              {:dependencies [[org.clojure/test.check "0.9.0"]
